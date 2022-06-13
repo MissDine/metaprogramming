9bb18601a54c7ea415151082702fba253841b5b0
MissDine <enidnyatichi1212@gmail.com>
2022-06-13 09:36:06 +0300

    Feature edit

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-# metaprogramming allows us to essentially write code that writes code.
+# metaprogramming allows us to essentially write code that writes code or modifies code.
+puts "Hello".reverse
+# Send sneds a message
+puts "Hello".send(:reverse)
+
+arg = :reverse
+puts "Hello".send(arg)
+
+# gsub substitutes
+puts "Hello".gsub("h","j")
